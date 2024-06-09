975d817e6f719fa0ee42535904e82714047f3bab
SystemX <SystemX@DESKTOP-F94OBE7>
2024-06-09 22:36:58 +0300

    Добавьте файлы проекта.

---
+++ trpo/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using OxyPlot.Series;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trpo
+{
+    internal class MainViewModel
+    {
+        public PlotModel PlotModel1 { get; set; }
+        public PlotModel PlotModel2 { get; set; }
+
+        public MainViewModel()
+        {
+            PlotModel1 = new PlotModel { Title = "График функции" };
+
+            var series1 = new LineSeries { Title = "Мой график" };
+            for (double x = 0; x <= 100; x += 1)
+            {
+                double y = Math.Log(x);
+
+                series1.Points.Add(new DataPoint(x, y));
+            }
+
+            PlotModel1.Series.Add(series1);
+
+            PlotModel2 = new PlotModel { Title = "График функции" };
+
+            var series2 = new LineSeries { Title = "Мой график" };
+            for (double x = 0; x <= 100; x += 1)
+            {
+                double y = x*x;
+
+                series2.Points.Add(new DataPoint(x, y));
+            }
+
+            PlotModel2.Series.Add(series2);
+        }
+    }
+}
---
+++ trpo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace trpo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void btnPaint_Click(object sender, RoutedEventArgs e)
+        {
+            PlotModel pm = new PlotModel();
+            Graph1.Model = pm;
+        }
+    }
+}
