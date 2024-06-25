6a772c90206a3fa85ce2ce4cddb118ed7c9b3f02
SystemX <SystemX@DESKTOP-F94OBE7>
2024-06-25 10:09:12 +0300

    Почти доделал

--- trpo/MainViewModel.cs
+++ trpo/MainViewModel.cs
@@ -5,39 +5,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot.Wpf;
 
 namespace trpo
 {
     internal class MainViewModel
     {
-        public PlotModel PlotModel1 { get; set; }
-        public PlotModel PlotModel2 { get; set; }
-
-        public MainViewModel()
+        public PlotModel plotModel { get; set; }
+        public MainViewModel(int count)
         {
-            PlotModel1 = new PlotModel { Title = "График функции" };
-
-            var series1 = new LineSeries { Title = "Мой график" };
-            for (double x = 0; x <= 100; x += 1)
+            plotModel = new PlotModel { Title = "Сравнение методов сортировки"};
+            var seriesBinari = new LineSeries 
+            { 
+                Title = "Сортировка бинарными вставками O(n*ln(n))",
+                Color = OxyColor.FromRgb(0, 128, 0)
+            };
+            for (double x = 0; x <= count; x += 1)
             {
-                double y = Math.Log(x);
+                double y = x * Math.Log(x);
 
-                series1.Points.Add(new DataPoint(x, y));
+                seriesBinari.Points.Add(new DataPoint(x, y));
             }
 
-            PlotModel1.Series.Add(series1);
-
-            PlotModel2 = new PlotModel { Title = "График функции" };
-
-            var series2 = new LineSeries { Title = "Мой график" };
-            for (double x = 0; x <= 100; x += 1)
+            var seriesSimple = new LineSeries 
             {
-                double y = x*x;
+                Title = "Сортировка простыми вставками O(n^2)",
+                Color = OxyColor.FromRgb(255, 0, 0)
+            };
+            for (double x = 0; x <= count; x += 1)
+            {
+                double y = x * x;
 
-                series2.Points.Add(new DataPoint(x, y));
+                seriesSimple.Points.Add(new DataPoint(x, y));
             }
 
-            PlotModel2.Series.Add(series2);
+            plotModel.Series.Add(seriesBinari);
+            plotModel.Series.Add(seriesSimple);
+
+            plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                Title = "Кол-во элементов"
+            });
+            plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                Title = "Кол-во перестановок"
+            });
+
+
         }
     }
 }
--- trpo/MainWindow.xaml.cs
+++ trpo/MainWindow.xaml.cs
@@ -25,13 +25,147 @@ namespace trpo
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
 
         private void btnPaint_Click(object sender, RoutedEventArgs e)
         {
-            PlotModel pm = new PlotModel();
-            Graph1.Model = pm;
+            try
+            {
+                int count = int.Parse(txtCount.Text);
+                if (count <= 0) 
+                {
+                    throw new Exception();
+                }
+                MainViewModel mvm = new MainViewModel(count);
+                PlotModel pm = mvm.plotModel;
+                Graph.Model = pm;
+            }
+            catch 
+            {
+                MessageBox.Show("Ошибка ввода количества элементов массива"); 
+            }
+        }
+
+        private async void btnSort_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> array = new List<int>();
+            string[] numbers = txtArray.Text.Split(' ');
+            
+            if (numbers.Length > 10)
+            {
+                MessageBox.Show("Слишком большой массив");
+                return;
+            }
+
+            foreach (string numberStr in numbers)
+            {
+                if (int.TryParse(numberStr, out int number))
+                {
+                    array.Add(number);
+                    ListBoxNumbers.Items.Add(number);
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка ввода массива");
+                    return;
+                }
+            }
+
+            if (rbSimpleSort.IsChecked == true)
+            {
+                await InsertionSort(array);
+            }
+            else if (rbBinariSort.IsChecked == true)
+            {
+                await BinaryInsertionSort(array);
+            }
+        }
+
+        private async Task InsertionSort(List<int> numbers)
+        {
+            int n = numbers.Count;
+            for (int i = 1; i < n; i++)
+            {
+                int key = numbers[i];
+                txtCurrentElement.Text = key.ToString();
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] > key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    numbers[j] = int.MinValue;
+                    j--;
+
+                    // Обновляем отображение в ListBox
+                    UpdateListBox(numbers);
+                    ListBoxNumbers.SelectedIndex = j + 1;
+                    ListBoxNumbers.ScrollIntoView(ListBoxNumbers.SelectedItem);
+                    await Task.Delay(1000); // Задержка для визуализации
+                }
+                numbers[j + 1] = key;
+
+                // Обновляем отображение в ListBox
+                UpdateListBox(numbers);
+                ListBoxNumbers.SelectedIndex = j + 1;
+                ListBoxNumbers.ScrollIntoView(ListBoxNumbers.SelectedItem);
+                await Task.Delay(1000); // Задержка для визуализации
+            }
+        }
+
+        private async Task BinaryInsertionSort(List<int> numbers)
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                int key = numbers[i];
+                txtCurrentElement.Text = key.ToString();
+                int left = 0;
+                int right = i - 1;
+
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (key < numbers[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+
+                for (int j = i - 1; j >= left; j--)
+                {
+                    numbers[j + 1] = numbers[j];
+
+                    numbers[j] = int.MinValue;
+                    // Обновляем отображение в ListBox
+                    UpdateListBox(numbers);
+                    ListBoxNumbers.SelectedIndex = j + 1;
+                    ListBoxNumbers.ScrollIntoView(ListBoxNumbers.SelectedItem);
+                    await Task.Delay(1000); // Задержка для визуализации
+                }
+
+                numbers[left] = key;
+
+                // Обновляем отображение в ListBox
+                UpdateListBox(numbers);
+                ListBoxNumbers.SelectedIndex = left;
+                ListBoxNumbers.ScrollIntoView(ListBoxNumbers.SelectedItem);
+                await Task.Delay(1000); // Задержка для визуализации
+            }
+        }
+
+        private void UpdateListBox(List<int> numbers)
+        {
+            ListBoxNumbers.Items.Clear();
+            foreach (var number in numbers)
+            {
+                if (number == int.MinValue)
+                    ListBoxNumbers.Items.Add(" ");
+                else
+                    ListBoxNumbers.Items.Add(number);
+            }
         }
     }
 }
